8b6436e873a751a132763fc0d08c3933e2e3ec3a
tmarciniakm <tmarciniakm@gmail.com>
2016-10-10 14:34:47 +0200

    basic controllers

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class AuthController extends Controller
+{
+    public function store(Request $request)
+    {
+        return 'It wurks';
+    }
+
+    public function signin(Request $request)
+    {
+        return 'It wurks';
+    }
+}
